a2ea415f51326550d7fb4647eb43ff06c5ffb777
Seralyn Campbell <seralyncampbell@gmail.com>
2022-04-26 19:44:31 +0900

    got API call dynamically generated by user input and converted all DOM manipulation via vanilla JS to the React Way

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import SearchBar from "./components/SearchBar";
 import { useState } from "react";
 
 function App() {
-  const [a, setA] = useState("");
+    const [cityName, setCityName] = useState("");
 
-  return (
-    <div className="App">
-      <Header />
-      <SearchBar setAFunc={setA} onCityChange="56" />
-      <Body a={a} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <SearchBar setCityName={setCityName} />
+            <Body cityName={cityName} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,33 +2,31 @@ import React from "react";
 import { useState } from "react";
 
 export default function SearchBar(props) {
-  const [enteredCity, setEnteredCity] = useState("");
+    const [enteredCity, setEnteredCity] = useState("");
 
-  const cityInputHandler = (event) => {
-    setEnteredCity(event.target.value);
-  };
+    const cityInputHandler = (event) => {
+        setEnteredCity(event.target.value);
+    };
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-    // const cityName = enteredCity;
-    // props.onCityChange(cityName);
-    setEnteredCity("");
-  };
-  console.log(props);
-  return (
-    <div className="mt-6 flex justify-center">
-      <form onSubmit={submitHandler}>
-        <input
-          //onChange={cityInputHandler}
-          onChange={(e) => props.setAFunc(e.target.value)}
-          className="rounded-md w-96 px-1 h-8"
-          type="text"
-          //   value={enteredCity}
-        />{" "}
-        <button type="submit" class="btn ml-3">
-          {props.onCityChange}
-        </button>
-      </form>
-    </div>
-  );
+    const submitHandler = (event) => {
+        event.preventDefault();
+        props.setCityName(enteredCity);
+        setEnteredCity("");
+    };
+
+    return (
+        <div className="mt-6 flex justify-center">
+            <form onSubmit={submitHandler}>
+                <input
+                    className="rounded-md w-96 px-1 h-8"
+                    type="text"
+                    onChange={cityInputHandler}
+                    value={enteredCity}
+                />
+                <button type="submit" value="Submit" className="btn ml-3">
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
 }
