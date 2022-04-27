04811d5337256b977d4ec6808210e7de5ae1498a
Seralyn Campbell <seralyncampbell@gmail.com>
2022-04-27 17:25:17 +0900

    added new logo situation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
+import sun from "../img/sunny-day.png";
 
 const Header = () => {
     return (
         <div className="text-5xl py-6 bg-[rgb(94, 96, 105)] text-gray-800 flex mt-5">
-            <div className="ml-10">YourWeatherNow</div>
+            <div className="ml-10">
+                YourWeatherNow
+                <img src={sun} className="inline-block ml-3 h-12" alt="" />
+            </div>
         </div>
     );
 };
