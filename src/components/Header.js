fc586f1fcabd38a3933c16d4a0b252fd53c2fb1e
Seralyn Campbell <seralyncampbell@gmail.com>
2022-04-27 17:17:53 +0900

    changed title of page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div className="text-5xl py-6 bg-[rgb(94, 96, 105)] text-gray-800 flex">
-            <div className="ml-10">Weather Dashboard</div>
+        <div className="text-5xl py-6 bg-[rgb(94, 96, 105)] text-gray-800 flex mt-5">
+            <div className="ml-10">YourWeatherNow</div>
         </div>
     );
 };
